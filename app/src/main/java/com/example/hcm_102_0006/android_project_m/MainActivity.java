ac3e6299d6a079b2ee5bf31c0a9be2200c10954e
haiminhtran810 <haiminhtran810@gmail.com>
2017-11-20 11:00:57 +0700

    Android Init Movie App

---
+++ app/src/main/java/com/example/hcm_102_0006/android_project_m/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hcm_102_0006.android_project_m;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
